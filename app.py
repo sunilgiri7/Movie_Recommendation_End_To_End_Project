7183c14b8c4f6d692d043774c721d6165ce09dad
user name <user email>
2022-11-20 00:45:43 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st 
+import pickle
+import requests
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=5605c559b505f5579e4630e2917687a3&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" +data['poster_path']
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    
+    recommended_movies_names = []
+    recommended_movies_posters = []
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+
+        # Fetch poster from api's
+        recommended_movies_names.append(movies.iloc[i[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies_names, recommended_movies_posters
+
+
+movies = pickle.load(open('movies.pkl','rb'))
+movies_list = movies['title']
+st.title("Movie Recommender System")
+Selected_movie_name = st.selectbox(
+    'How Would You Like To Be Conducted',
+    movies_list
+)
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+if st.button('Recommend'):
+    recommended_movie_names,recommended_movie_posters = recommend(Selected_movie_name)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(recommended_movie_names[0])
+        st.image(recommended_movie_posters[0])
+    with col2:
+        st.text(recommended_movie_names[1])
+        st.image(recommended_movie_posters[1])
+
+    with col3:
+        st.text(recommended_movie_names[2])
+        st.image(recommended_movie_posters[2])
+    with col4:
+        st.text(recommended_movie_names[3])
+        st.image(recommended_movie_posters[3])
+    with col5:
+        st.text(recommended_movie_names[4])
+        st.image(recommended_movie_posters[4])
